780f9f681547d00a3aae5024592f0c2631f15c35
yjaouhar <yassinejaouhary82@gmail.com>
2024-12-18 11:08:35 +0100

    declarations

--- declarations.js
+++ declarations.js
@@ -1,11 +1,14 @@
 const escapeStr = '\`\\\/\"\'';
 const arr =[4,'2'];
+Object.freeze(arr);
 const obj = {
     str : 'hello' ,
     num : 10 ,
     bool : true ,
     undef : undefined
 }
+Object.freeze(obj);
+
 const nested = {
     arr :[4,undefined,'2'],
     obj :{
@@ -15,7 +18,9 @@ const nested = {
     }
 }
 
-Object.freeze(obj);
-Object.freeze(arr);
 Object.freeze(nested)
+Object.freeze(nested.arr)
+Object.freeze(nested.obj)
 
+
+console.log(nested.arr)
