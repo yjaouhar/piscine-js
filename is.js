b3266f33729a376f3e8689d15461b9c3cbf8cf49
yjaouhar <yassinejaouhary82@gmail.com>
2024-12-18 17:21:26 +0100

    is

---
+++ is.js
@@ -0,0 +1,36 @@
+const is={}
+    is.num= function(number){
+        return  typeof number == "number"
+    };
+    is.nan =function(namber){
+        return  Number.isNaN(namber)
+    };
+    is.str = function(str){
+         return  typeof str == "string"
+    };
+    is.bool = function(arg){
+            return typeof arg=="boolean"
+    };
+    is.undef = function(undef){
+            return typeof undef=="undefined"
+    };
+    is.def = function(def){
+            return (typeof def=="undefined" || def!=null)
+    };
+    is.arr = function(arr){
+            return  Array.isArray(arr)
+    };
+    is.obj = function(obj){
+            return (obj!=null &&!is.arr&&typeof obj == "object" )
+    };
+    is.fun = function(fun){
+            return typeof fun=="function"
+    };
+    is.truthy = function(arg){
+            return arg?true:false
+    };
+    is.falsy = function(falsy){
+            return !this.truthy(falsy)
+    };
+
+
