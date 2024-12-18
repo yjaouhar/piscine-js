4052d6cb7628927995c61e1b58c5fe92a492215b
yjaouhar <yassinejaouhary82@gmail.com>
2024-12-18 12:01:54 +0100

    more-or-less

---
+++ more-or-less.js
@@ -0,0 +1,12 @@
+function more(num) {
+    return num+1
+}; 
+function less(num) {
+    return num-1
+}; 
+function add(num1,num2) {
+    return num1+num2
+}; 
+function sub(num1,num2) {
+    return num1-num2
+}; 
