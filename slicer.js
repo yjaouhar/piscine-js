3a77056e491f1b200552b9d07f79d7cd366f3d37
yjaouhar <yassinejaouhary82@gmail.com>
2024-12-20 16:40:02 +0100

    slice

--- slicer.js
+++ slicer.js
@@ -2,6 +2,7 @@ function slice(arr,from,ending){
     let start = 0
     let end = arr.length
     let res = ""
+    let array=[]
       if (typeof from!=="undefined"){
         start = from
       }
@@ -11,7 +12,12 @@ function slice(arr,from,ending){
     if (end>=0&&start>=0){
        
       for (let i = start; i < end; i++) {
-        res+=arr[i]
+        if (Array.isArray(arr)){
+            array.push(arr[i])
+        }else{
+            res+=arr[i]
+        }
+        
     }  
     }else{
         if(end<0){
@@ -25,13 +31,16 @@ function slice(arr,from,ending){
             end=0
         }    
         for (let i = start; i<(arr.length)-end; i++) {
-            res+=arr[i]  
+            if (Array.isArray(arr)){
+                array.push(arr[i])
+            }else{
+                res+=arr[i]
+            }
+            
         }
     } 
     if (Array.isArray(arr)){
-        res=res.split("")
+        return array
     }
     return res
 };
-
-
