31b034cdd5b8ba1a95f601b070c2470af6ab3792
yjaouhar <yassinejaouhary82@gmail.com>
2024-12-20 16:32:59 +0100

    :

--- repeat.js
+++ repeat.js
@@ -5,3 +5,4 @@ function repeat(str,num){
     }
     return rep
 };
+
---
+++ slicer.js
@@ -0,0 +1,33 @@
+function slice(arr,from,ending){
+    let start = 0
+    let end = arr.length
+    let res = ""
+      if (typeof from!=="undefined"){
+        start = from
+      }
+      if (typeof ending!=="undefined"){
+        end = ending
+      }
+    if (end>=0&&start>=0){
+       
+      for (let i = start; i < end; i++) {
+        res+=arr[i]
+    }  
+    }else{
+        if(end<0){
+            end=-end
+        }   
+        if (start<0){
+            start=-start
+        }
+        if (end==arr.length){
+            start=end-start
+            end=0
+        }    
+        for (let i = start; i<(arr.length)-end; i++) {
+            res+=arr[i]  
+        }
+    } 
+    return res
+};
+
