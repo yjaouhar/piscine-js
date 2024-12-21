2881e9700f73392eb62087a57a83a35c71569ee1
Yassine Jaouhary <yjaouhar@noreply.learn.zone01oujda.ma>
2024-12-21 09:02:35 +0000

    Update 'cut-corners.js'

--- cut-corners.js
+++ cut-corners.js
@@ -1,10 +1,84 @@
 function round(arr){
-    let res = 0
-for (let i=0;i<1;i++){
- res =arr[i]
-}
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]>0){
+                let res = sep(arr[i])
+                 let n = arr[i]-res
 
-return res
+            if (n<0.5){
+                arr[i]=res
+            }else{
+                arr[i]=res+1
+            }
+        }else{
+            let res = sep(arr[i])
+            let n = res-arr[i]  
+            if (n<=0.5){
+                arr[i]=res
+            }else{
+                arr[i]=res-1
+            }
+        }
+    }
+
+return arr
 };
 
-console.log(round([3.7, -3.7, 3.1, -3.1]))
+
+function trunc(arr){
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]>0){
+            let res = sep(arr[i])
+            arr[i]=res
+        }else{
+            let res = sep(arr[i])
+            arr[i]=res
+        }
+    }
+return arr
+};
+
+function floor(arr){
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]>0){
+            let res = sep(arr[i])
+            arr[i]=res
+        }else{
+            let res = sep(arr[i])
+            arr[i]=res-1
+        }
+    }
+return arr
+};
+function ceil(arr){
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]>0){
+            let res = sep(arr[i])
+            arr[i]=res+1
+        }else{
+            let res = sep(arr[i])
+            arr[i]=res
+        }
+        
+    }
+return arr
+};
+
+
+
+function sep(num){
+    let n=0
+    let isN = false
+    if (num<0){
+       
+        num=-num
+        isN = true
+    }
+    for (let i = 0; i <= num; i++) {
+        n=i   
+    }
+    if (isN===true){
+        n=-n
+    }
+    return n
+};
