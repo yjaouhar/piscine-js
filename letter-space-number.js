b4476939f5771fdfc9c14e6478ac514ea3d135f3
yjaouhar <yassinejaouhary82@gmail.com>
2024-12-22 12:42:43 +0100

    letter-space-number

--- letter-space-number.js
+++ letter-space-number.js
@@ -2,9 +2,12 @@ function letterSpaceNumber(str) {
     let arr=[]
     let res = /[A-Za-z] \d\b/g
     arr=str.match(res)
+    // console.log(arr);
+    
     if (arr===null){
-        return null
+        return []
     }
     return arr 
 };
 
+console.log(letterSpaceNumber("example 1d, example 20"));
