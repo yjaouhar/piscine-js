448b79d298f62c2efab2a9b115b63c80d004d5ac
Yassine Jaouhary <yjaouhar@noreply.learn.zone01oujda.ma>
2024-12-22 08:48:49 +0000

    Upload files to ''

---
+++ triangle.js
@@ -0,0 +1,17 @@
+function triangle(sep, n) {
+let str=""
+
+    for (let i = 0; i < n; i++) {
+        if (i!==0){
+            str+="\n" 
+        }
+        for (let j = 0; j<i; j++) {
+           
+            str+=sep
+        }
+        
+    }
+    return str
+};
+
+console.log(triangle('*', 10));
